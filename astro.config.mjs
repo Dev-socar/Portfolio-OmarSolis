5e7d3b7ededa12a782a1e290a6f2fbf359a94374
Dev-socar <omar.fsc19@gmail.com>
2026-02-28 17:35:47 -0600

    fix:Se actualizo astro a su version 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
-  site: "https://omarsolisdev.com/",
-  integrations: [
-    tailwind(),
-    sitemap({
-      // Esta función permite ajustar los datos de cada página individualmente
-      serialize(item) {
-        if (/index/.test(item.url)) {
-          item.priority = 1.0;
-          item.changefreq = "weekly";
-        } else if (/proyectos/.test(item.url)) {
-          item.priority = 0.9;
-          item.changefreq = "weekly";
-        } else {
-          item.priority = 0.7;
-          item.changefreq = "yearly";
-        }
-        item.lastmod = new Date("2026-02-28").toISOString();
-        return item;
+  site: "https://omarsolisdev.com",
+  integrations: [tailwind(), sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        "@src": fileURLToPath(new URL("./src", import.meta.url)),
       },
-    }),
-  ],
+    },
+  },
 });
